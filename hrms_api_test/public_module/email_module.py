a82b7ec7995db441f25593b94e8c4411d2713a81
evaliu <1872863416@qq.com>
2021-03-02 10:42:06 +0800

    update

---
+++ hrms_api_test/public_module/email_module.py
@@ -0,0 +1,6 @@
+# -*- coding:utf-8 -*-
+"""
+邮件操作模块
+"""
+
+from hrms_api_test.public_module.logger_module import logger
---
+++ hrms_api_test/public_module/output_report_module.py
@@ -0,0 +1,6 @@
+# -*- coding:utf-8 -*-
+"""
+输出报告模块
+"""
+
+from hrms_api_test.public_module.logger_module import logger
